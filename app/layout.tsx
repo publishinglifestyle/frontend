f72c058e83f0b67d91ac6af731f1580bc3353cb8
Brian Donatiello <briandonatiello@Brians-MacBook-Pro.local>
2024-07-17 10:53:06 +0200

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
             <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+            <main className="w-full max-w-screen-4xl mx-auto pt-16 px-6 flex-grow">
               {children}
             </main>
             <footer className="w-full flex items-center justify-center py-3">
@@ -60,7 +60,7 @@ export default function RootLayout({
                 title="nextui.org homepage"
               >
                 <span className="text-default-600">Copywright @</span>
-                <p className="text-secondary">Riccard Mazza</p>
+                <p className="text-secondary">Low Content Academy</p>
               </Link>
             </footer>
           </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -90,12 +90,12 @@ export const Navbar = () => {
   }
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
+    <NextUINavbar maxWidth="2xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">Riccardo Mazza</p>
+            <p className="font-bold text-inherit" style={{ fontSize: "25px" }}>Low Content AI</p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
