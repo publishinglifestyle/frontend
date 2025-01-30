8e471a3d9dabf1efeef02f30259d3515b8da9430
Brian Donatiello <briandonatiello@gmail.com>
2025-01-30 12:45:29 +0100

    mandala negative

--- app/modals/ideogramModal.tsx
+++ app/modals/ideogramModal.tsx
@@ -102,7 +102,7 @@ const IdeogramModal: React.FC<IdeogramModalProps> = ({
 
     detectLanguage();
 
-    if (agentId == "88ab4ff2-dca1-4753-b3f4-2f34b564e4a5" || agentId == "10280eaa-370c-4456-a6f6-a182f3605d67" || agentId == "e2c5ee2c-3130-4603-85ab-e2a2f88d9470") {
+    if (agentId == "88ab4ff2-dca1-4753-b3f4-2f34b564e4a5" || agentId == "10280eaa-370c-4456-a6f6-a182f3605d67" || agentId == "e2c5ee2c-3130-4603-85ab-e2a2f88d9470" || agentId == "73afdc60-c72f-458d-a5fa-af9ceeeeb761" || agentId == "35563d6e-da4c-4042-a793-ec68e1812dbe") {
       let negative_prompt = (agentId == "88ab4ff2-dca1-4753-b3f4-2f34b564e4a5") ? "No thin lines. No fine details. No intricate textures. No delicate strokes. No light or faint outlines. No shading. No complex patterns. No background elements. No realism. No soft edges." : "no details"
       setNegativePrompt(negative_prompt)
     }
