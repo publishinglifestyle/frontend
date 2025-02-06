64c4c612177cc9ab72534f706cb7b8f50f0cce67
Brian Donatiello <briandonatiello@gmail.com>
2025-02-06 17:55:02 +0400

    fix

--- app/modals/ChatModals.tsx
+++ app/modals/ChatModals.tsx
@@ -143,7 +143,7 @@ const ChatModals = ({
       />
 
       <CommandsModal
-        agentId={selectedAgent?.id}
+        agentId={selectedAgent?.id || ""}
         isOpen={isCommandsModalOpen}
         onClose={() => setIsCommandsModalOpen(false)}
         onSuccess={(selected_commands) => {
