8011ed176d2218c2b2585f7cd8f5309c0a7320d2
Brian Donatiello <briandonatiello@gmail.com>
2025-06-10 18:11:15 +0200

    new meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,7 +101,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1267228664624071');
+              fbq('init', '1242583274036702');
               fbq('track', 'PageView');
             `,
           }}
@@ -111,7 +111,7 @@ export default function RootLayout({
             height="1" 
             width="1" 
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1267228664624071&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1242583274036702&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
